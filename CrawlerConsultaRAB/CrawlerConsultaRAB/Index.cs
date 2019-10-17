0f813a7d19a1c91ab11ab52e8c52c255f503e640
Snyp <gabrielmarques.mg@gmail.com>
2019-10-17 08:24:05 -0300

    New

---
+++ CrawlerConsultaRAB/CrawlerConsultaRAB/Index.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrawlerConsultaRAB
+{
+    class Index
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CrawlerConsultaRAB/CrawlerConsultaRAB/Navigator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrawlerConsultaRAB
+{
+    public class Navigator
+    {
+    }
+}
